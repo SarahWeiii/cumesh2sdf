2b459536300f8d4697ffb284cbf0a62563128aa1
Xinyue Wei <13659185706@163.com>
2024-01-10 20:59:51 -0800

    fix sdf, add quad divide

--- test.py
+++ test.py
@@ -9,6 +9,11 @@ from diso import DiffDMC, DiffMC
 import glob
 import time
 
+def tri_area(v0, v1, v2):
+    ab = v1 - v0
+    ac = v2 - v0
+    return 0.5 * numpy.linalg.norm(numpy.cross(ab, ac), axis=1)
+
 # vol2mesh params
 vol2mesh = DiffDMC(dtype=torch.float32).cuda()
 
@@ -30,7 +35,7 @@ def load_and_preprocess(p):
 
 meshFiles = glob.glob('/home/sarahwei/dataset/thingi10k_160/*.stl')
 # meshFiles = ["tmp/spot.obj"]
-out_dir = "output256/"
+out_dir = "output256_v3/"
 
 for meshFile in meshFiles:
     tris, tris_min, tris_max = load_and_preprocess(meshFile)
@@ -43,7 +48,7 @@ for meshFile in meshFiles:
     start = time.perf_counter()
     start1 = time.perf_counter()
     d = torchcumesh2sdf.get_sdf(tris, R, band)
-    if d.min() >= 0:
+    if d.min() >= -0.5 / R:
         print("Using UDF!")
         file.write("Using UDF!\n")
         d = d - 0.9 / R
@@ -66,5 +71,20 @@ for meshFile in meshFiles:
     v, f = v.cpu().numpy(), f.cpu().numpy()
     v = ((v * R +0.5)/(R+1)* margin - band) * tris_max + tris_min
 
+    # divide quads into triangles by traingles area
+    area_012 = tri_area(v[f[:,0]], v[f[:,1]], v[f[:,2]])
+    area_023 = tri_area(v[f[:,0]], v[f[:,2]], v[f[:,3]])
+    area_013 = tri_area(v[f[:,0]], v[f[:,1]], v[f[:,3]])
+    area_123 = tri_area(v[f[:,1]], v[f[:,2]], v[f[:,3]])
+    faces_1 = f[numpy.abs(area_012-area_023) > numpy.abs(area_013-area_123)]
+    faces_2 = f[numpy.abs(area_012-area_023) <= numpy.abs(area_013-area_123)]
+    f = numpy.concatenate([faces_1[:, [0, 1, 3, 1, 2, 3]].reshape(-1, 3), faces_2[:, [0, 1, 2, 0, 2, 3]].reshape(-1, 3)], axis=0)
+
     mesh = trimesh.Trimesh(vertices=v, faces=f)
     mesh.export(out_dir + meshFile.split('/')[-1].split('.')[0] + ".obj")
+
+    # v, f = mcubes.marching_cubes(d.cpu().numpy(), 0)
+    # print(v.max(), v.min())
+    # v = ((v+0.5) / R * margin - band) * tris_max + tris_min
+    # mcubes.export_obj(v, f, "tmp/test.obj")
+
